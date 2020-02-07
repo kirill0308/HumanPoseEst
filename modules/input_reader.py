1b36c8b17e517d885dea2a9dcad7b2cf3fbcfc7f
Ilya from Yuan <guruilya86@gmail.com>
2020-02-07 17:01:50 +0800

    updated input_reader for realsense intel camera

--- modules/input_reader.py
+++ modules/input_reader.py
@@ -1,5 +1,5 @@
 import cv2
-
+import pyrealsense2 as rs
 
 class ImageReader:
     def __init__(self, file_names):
@@ -43,13 +43,19 @@ class VideoReader:
         return img
 
 class VideoReaderFromIntelRealsenseCAM:
-    def __init__(self, file_name):
-        self.file_name = file_name
-        try:  # OpenCV needs int to read from webcam
-            self.file_name = int(file_name)
-        except ValueError:
-            pass
+    def __init__(self, frame_width, frame_height, CAM_FPS):
+        self.pipeline = rs.pipeline()
+        config = rs.config()
+        config.enable_stream(rs.stream.depth, frame_width, frame_height, rs.format.z16, CAM_FPS)
+        config.enable_stream(rs.stream.color, frame_width, frame_height, rs.format.bgr8, CAM_FPS)
 
+        # color_path = 'temp/V00P00A00C00_rgb.avi'
+        # depth_path = 'temp/V00P00A00C00_depth.avi'
+        # colorwriter = cv2.VideoWriter(color_path, cv2.VideoWriter_fourcc(*'XVID'), CAM_FPS, (frame_width, frame_height),
+        #                               1)
+        # depthwriter = cv2.VideoWriter(depth_path, cv2.VideoWriter_fourcc(*'XVID'), CAM_FPS, (frame_width, frame_height),
+        #                               1)
+        self.pipeline.start(config)
     def __iter__(self):
         self.cap = cv2.VideoCapture(self.file_name)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
